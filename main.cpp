15c420b022e7646dd8568e5e40740fd66425b283
Marius <m.gzegozevskis@gmail.com>
2023-02-09 12:05:52 +0200

    pridejau dar naujesne

--- main.cpp
+++ main.cpp
@@ -36,6 +36,11 @@ void StudentoKurimas(vector<Studentas> &studentai){
     cin >> amzius;
     studentai.push_back(Studentas("sawee",vardas, amzius,"aawae"));
 }
+void Spausdinti(){
+    for (int i = 0; i < 11; ++i) {
+        cout << "labas";
+    }
+}
 
 
 int main() {
